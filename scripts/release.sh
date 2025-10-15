3a12f0dc7253f414235a2a46bb4fc4a2f0257e27
Håvard Ottestad <hmottestad@gmail.com>
2025-10-15 10:15:31 +0200

    update pom after release

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,7 +6,7 @@ cd ..
 echo "Ready to release to maven central?"
 read -n 1 -srp "Press any key to continue (ctrl+c to cancel)"; printf "\n\n";
 
-mvn clean deploy -P maven-central -DskipTests
+mvn clean deploy -P central -DskipTests
 
 
 echo "Run the following command to clean up after the release:"
